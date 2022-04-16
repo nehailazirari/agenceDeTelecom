4aec7059db98aa15d4768a2ebe4c5e63f6a40321
Lamiaa12 <92425353+Lamiaa12@users.noreply.github.com>
2022-04-16 09:30:06 +0000

    hta t9adiha "la hsab l projet diyalak

--- src/main/java/controller/ControllerAccueil.java
+++ src/main/java/controller/ControllerAccueil.java
@@ -1,4 +1,86 @@
 package controller;
 
-public class ControllerAccueil {
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ControllerAccueil{
+
+    @FXML
+    private Button bouton_client;
+
+    @FXML
+    private Button bouton_facture;
+
+    @FXML
+    private Button boutton_forfait;
+    @FXML
+    private ImageView image_client;
+
+    @FXML
+    private ImageView image_facture;
+
+    @FXML
+    private ImageView image_forfait;
+
+
+    @FXML
+    void acceder_client(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("/Layout/Client.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(tableViewScene);
+        window.show();
+
+    }
+
+    @FXML
+    void acceder_facture(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("/Layout/Facture.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(tableViewScene);
+        window.show();
+
+
+    }
+
+    @FXML
+    void acceder_forfait(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("/Layout/Forfait.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(tableViewScene);
+        window.show();
+
+    }
+    //Chargement des images
+
+   @FXML
+    public void initialize(){
+        image_client.setImage(new Image("C:\\Users\\HP\\Documents\\EMI1\\Projet2\\photos\\client.png") );
+       image_forfait.setImage(new Image("C:\\Users\\HP\\Documents\\EMI1\\Projet2\\photos\\telephone.png") );
+       image_facture.setImage(new Image("C:\\Users\\HP\\Documents\\EMI1\\Projet2\\photos\\facturer.png") );
+    }
+
 }
+
